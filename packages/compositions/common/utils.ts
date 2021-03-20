1c19a3779497be834c1132a5bf095bf87c98e5e4
wujc <15755081023@163.com>
2021-03-20 23:25:25 +0800

    123

--- packages/compositions/common/utils.ts
+++ packages/compositions/common/utils.ts
@@ -2,5 +2,7 @@ export default function isInMac(): boolean {
     console.log(123);
     console.log(123);
     console.log(123);
+    console.log(123);
+    console.log(123);
     return /macintosh|mac os x/i.test(navigator.userAgent);
 }
---
+++ packages/compositions/index.ts
@@ -0,0 +1,8 @@
+export default function isInMac(): boolean {
+  console.log(123);
+  console.log(123);
+  console.log(123);
+  console.log(123);
+  console.log(123);
+  return /macintosh|mac os x/i.test(navigator.userAgent);
+}
