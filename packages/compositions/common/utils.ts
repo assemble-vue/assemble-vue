cc01770d577ab39c5243a0f3457086044d2f2e3f
wujc <15755081023@163.com>
2021-03-23 10:32:34 +0800

    feat(czinit): 初始化commitzen

--- packages/compositions/common/utils.ts
+++ packages/compositions/common/utils.ts
@@ -1,8 +1,3 @@
 export default function isInMac(): boolean {
-    console.log(123);
-    console.log(123);
-    console.log(123);
-    console.log(123);
-    console.log(123);
     return /macintosh|mac os x/i.test(navigator.userAgent);
 }
